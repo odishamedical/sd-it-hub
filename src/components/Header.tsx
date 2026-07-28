7243bf271f1b8f44a4df27bbd7881c481bda63c2
SD Ecosystem <admin@shyamdash.com>
2026-07-28 13:32:42 +0530

    style: make header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="absolute top-0 left-0 w-full z-[200] bg-[#050B1B]/80 backdrop-blur-md border-b border-slate-900">
+      <header className="fixed top-0 left-0 w-full z-[200] bg-[#050B1B]/80 backdrop-blur-md border-b border-slate-900">
         <div className="container mx-auto px-4 lg:px-8 h-20 flex items-center justify-between">
           
           {/* Logo */}
